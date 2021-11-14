f80e6a44772f2c81072637a97014fc2989ace4b5
Ho Quang Linh <hoquanglinh2710@gmail.com>
2021-11-14 15:34:11 +0700

    fix productByCategory

--- router/productRouter.js
+++ router/productRouter.js
@@ -55,8 +55,9 @@ router.post("/find", (req, res) => {
 //List product by id
 router.get("/productByCategory/", async (req, res) => {
   const category = req.query.category;
+  const name = req.query.name;
   if (category == "all") {
-    var products = await Product.find();
+    var products = await Product.find({ name: new RegExp(name, "i") });
     if (products) {
       res.status(200).send([{ productList: products }]);
     } else {

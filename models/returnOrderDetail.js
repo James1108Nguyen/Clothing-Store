2a2d67d7810b4d0f20d379efe2726f64f52a2022
Ho Quang Linh <hoquanglinh2710@gmail.com>
2021-12-02 09:53:26 +0700

    add returnOrder, returnOrder Detail

---
+++ models/returnOrderDetail.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+let returnOrderDetail_Schema = new Schema({
+  orderDetail: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "OrderDetail",
+    required: true,
+  },
+  returnedQuantity: {
+    type: Number,
+    required: true,
+  },
+});
+exports.ReturnOrderDetail = mongoose.model(
+  "ReturnOrderDetail",
+  returnOrderDetail_Schema
+);

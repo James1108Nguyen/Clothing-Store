3585a57e428ebb6f524ae1bee6217c7b85ff97d7
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2021-12-11 19:44:25 +0700

    Undo OrderRouter

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -5,13 +5,14 @@ const router = express.Router();
 const { Order } = require("../models/order");
 const { OrderDetail } = require("../models/order");
 const generateQR = require("../middlewares/gererateQR");
-const { DateTime } = require("luxon");
 const { cloudinary } = require("../config/cloudinary");
+
 router.get("/list", async (req, res) => {
   var orders = await Order.find()
     .populate({ path: "orderDetails" })
     .populate("customer", "name phone point")
-    .populate("user", "fullname");
+    .populate("user", "fullname")
+    .sort({ dateOrder: -1 });
   if (orders) {
     res.status(200).send(orders);
   } else {
@@ -58,10 +59,6 @@ router.post("/create", async (req, res) => {
     });
 });
 
-router.get("/test/TIme", async (req, res) => {
-  const time = DateTime.now().setZone("Asia/Ho_Chi_Minh");
-  res.send(time.toString());
-});
 router.post("/product/add", async (req, res) => {
   let orderDetail = OrderDetail({
     productId: req.body.productId,
@@ -91,6 +88,7 @@ router.post("/", async function (req, res) {
   newOrderDetails.map((orderDetail) => {
     orderDetailIds.push(orderDetail._id);
   });
+
   const order = new Order({
     user: req.body.user,
     customer: req.body.customer,
@@ -145,14 +143,25 @@ router.post("/", async function (req, res) {
 
 //get total revenue today
 router.get("/revenue/revenueToday", async function (req, res) {
+  var startOfDate = new Date();
+  var endOfDate = new Date();
+
+  startOfDate.setUTCHours(0, 0, 0, 0);
+  endOfDate.setUTCHours(23, 59, 59, 999);
   const agg = Order.aggregate([
     {
-      $match: { orderTotal: { $lte: 400000 } },
+      $match: {
+        dateOrder: {
+          $gte: startOfDate,
+          $lte: endOfDate,
+        },
+      },
     },
     {
       $group: {
         _id: null,
         total: { $sum: { $subtract: ["$orderTotal", "$totalReturnPrice"] } },
+        dateOrder: { $first: "$dateOrder" },
       },
     },
   ]).exec((err, doc) => {
@@ -163,5 +172,375 @@ router.get("/revenue/revenueToday", async function (req, res) {
     }
   });
 });
+//get top 5 product
+router.get("/revenue/getTop5ProductByRevenue", async function (req, res) {
+  var startOfDate = new Date();
+  var endOfDate = new Date();
 
+  startOfDate.setUTCHours(0, 0, 0, 0);
+  endOfDate.setUTCHours(23, 59, 59, 999);
+
+  const totalResult = await Order.aggregate([
+    {
+      $match: {
+        dateOrder: {
+          $gte: startOfDate,
+          $lte: endOfDate,
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "orderdetails", // update your actual product collection name
+        localField: "orderDetails",
+        foreignField: "_id",
+        as: "orderDetails",
+      },
+    },
+    { $unwind: "$orderDetails" },
+    {
+      $lookup: {
+        from: "products",
+        localField: "orderDetails.product",
+        foreignField: "_id",
+        as: "orderDetails.product",
+      },
+    },
+    { $unwind: "$orderDetails.product" },
+
+    {
+      $group: {
+        _id: "$orderDetails.product._id",
+        count: { $sum: "$orderDetails.quantity" },
+        totalSalePrice: {
+          $sum: {
+            $multiply: [
+              "$orderDetails.quantity",
+              "$orderDetails.product.salePrice",
+            ],
+          },
+        },
+        dateOrder: { $first: "$dateOrder" },
+      },
+    },
+    { $sort: { totalSalePrice: -1, count: -1 } },
+    { $limit: 5 },
+    {
+      $lookup: {
+        from: "products",
+        localField: "_id",
+        foreignField: "_id",
+        as: "product",
+      },
+    },
+    { $unwind: "$product" },
+    {
+      $project: {
+        _id: 1,
+        totalSalePrice: 1,
+        count: 1,
+
+        dateOrder: 1,
+        productName: "$product.name",
+        salePrice: "$product.salePrice",
+      },
+    },
+  ]).exec((err, doc) => {
+    if (doc) {
+      res.send(doc);
+    } else {
+      res.send(err);
+    }
+  });
+});
+//get top product by quantity
+router.get("/revenue/getTopProductByQuantity/:number", async (req, res) => {
+  var startOfDate = new Date();
+  var endOfDate = new Date();
+  const number = req.params.number;
+  startOfDate.setUTCHours(0, 0, 0, 0);
+  endOfDate.setUTCHours(23, 59, 59, 999);
+  console.log(req.params.number);
+  const totalResult = await Order.aggregate([
+    {
+      $match: {
+        dateOrder: {
+          $gte: startOfDate,
+          $lte: endOfDate,
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "orderdetails", // update your actual product collection name
+        localField: "orderDetails",
+        foreignField: "_id",
+        as: "orderDetails",
+      },
+    },
+    { $unwind: "$orderDetails" },
+    {
+      $lookup: {
+        from: "products",
+        localField: "orderDetails.product",
+        foreignField: "_id",
+        as: "orderDetails.product",
+      },
+    },
+    { $unwind: "$orderDetails.product" },
+
+    {
+      $group: {
+        _id: "$orderDetails.product._id",
+        count: { $sum: "$orderDetails.quantity" },
+        totalSalePrice: {
+          $sum: {
+            $multiply: [
+              "$orderDetails.quantity",
+              "$orderDetails.product.salePrice",
+            ],
+          },
+        },
+        dateOrder: { $first: "$dateOrder" },
+      },
+    },
+    { $sort: { count: -1, totalSalePrice: -1 } },
+    { $limit: parseInt(req.params.number) },
+    {
+      $lookup: {
+        from: "products",
+        localField: "_id",
+        foreignField: "_id",
+        as: "product",
+      },
+    },
+    { $unwind: "$product" },
+    {
+      $project: {
+        _id: 1,
+        totalSalePrice: 1,
+        count: 1,
+
+        dateOrder: 1,
+        productName: "$product.name",
+        salePrice: "$product.salePrice",
+      },
+    },
+  ]).exec((err, doc) => {
+    if (doc) {
+      res.send(doc);
+    } else {
+      res.send(err);
+    }
+  });
+});
+//get Expensive today
+router.get("/revenue/getExpensiveToday", function (req, res) {
+  var startOfDate = new Date();
+  var endOfDate = new Date();
+
+  startOfDate.setUTCHours(0, 0, 0, 0);
+  endOfDate.setUTCHours(23, 59, 59, 999);
+  Order.aggregate([
+    {
+      $match: {
+        dateOrder: {
+          $gte: startOfDate,
+          $lte: endOfDate,
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "orderdetails",
+        localField: "orderDetails",
+        foreignField: "_id",
+        as: "orderDetails",
+      },
+    },
+    {
+      $unwind: "$orderDetails",
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "orderDetails.product",
+        foreignField: "_id",
+        as: "orderDetails.product",
+      },
+    },
+    { $unwind: "$orderDetails.product" },
+    {
+      $group: {
+        _id: null,
+
+        totalExpensive: {
+          $sum: {
+            $multiply: [
+              "$orderDetails.product.originPrice",
+              "$orderDetails.quantity",
+            ],
+          },
+        },
+
+        dateOrder: { $first: "$dateOrder" },
+      },
+    },
+  ]).exec(function (err, doc) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      if (doc.length > 0) {
+        res.status(200).send(doc);
+      } else {
+        res.json({
+          totalExpensive: 0,
+          dateOrder: startOfDate,
+        });
+      }
+    }
+  });
+});
+function getMonday(d) {
+  d = new Date(d);
+  d.setUTCHours(0, 0, 0, 0);
+  var day = d.getDay(),
+    diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+  return new Date(d.setDate(diff));
+}
+
+router.get("/revenue/getCountOrderToday", function (req, res) {
+  var startOfDate = new Date();
+  var endOfDate = new Date();
+
+  startOfDate.setUTCHours(0, 0, 0, 0);
+  endOfDate.setUTCHours(23, 59, 59, 999);
+  Order.aggregate([
+    {
+      $match: {
+        dateOrder: {
+          $gte: startOfDate,
+          $lte: endOfDate,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        countOrder: { $sum: 1 },
+        dateOrder: { $first: "$dateOrder" },
+      },
+    },
+  ]).exec(function (err, doc) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(doc);
+    }
+  });
+});
+function randomDate(start, end) {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+}
+//create script random date in a week
+router.get("/revenue/updateOrder", async function (req, res) {
+  //Order.updateMany()
+  var orders = await Order.find();
+  orders.forEach((order) => {
+    console.log(order);
+    Order.findByIdAndUpdate(
+      order._id,
+      {
+        dateOrder: randomDate(new Date(2021, 11, 3), new Date()),
+      },
+      { new: true },
+      function (err, doc) {
+        if (err) {
+          // res.send(err);
+        } else {
+          //res.send(doc);
+        }
+      }
+    );
+  });
+  if (orders) {
+    res.send(orders);
+  }
+  // Order.updateMany(
+  //   {
+  //     dateOrder: randomDate(new Date(2021, 11, 5), new Date(2021, 11, 13)),
+  //   },
+
+  //   function (err, order) {
+  //     if (err) {
+  //       //res.send(err);
+  //     } else {
+  //       //res.send(order);
+  //     }
+  //   }
+  // );
+  console.log(randomDate(new Date(2012, 0, 1), new Date()));
+  //res.send(randomDate(new Date(2021, 11, 5), new Date(2021, 11, 13)));
+});
+router.get("/revenue/getTotalCustomerByThisWeek", function (req, res) {
+  const startOfWeek = getMonday(new Date());
+
+  startOfWeek.setUTCHours(0, 0, 0, 0);
+  Order.aggregate([
+    {
+      $match: {
+        dateOrder: {
+          $gte: startOfWeek,
+        },
+      },
+    },
+
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$dateOrder" } },
+        totalCustomer: { $sum: 1 },
+      },
+    },
+  ]).exec(function (err, doc) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+router.get("/revenue/getTotalCustomerByLastWeek", function (req, res) {
+  const startDate = new Date();
+  const endDate = new Date();
+  startDate.setDate(startDate.getDate() - 7);
+  const startOfWeek = getMonday(startDate);
+  const endOfWeek = getMonday(endDate);
+  console.log(endOfWeek);
+  startOfWeek.setUTCHours(0, 0, 0, 0);
+  Order.aggregate([
+    {
+      $match: {
+        dateOrder: {
+          $gte: startOfWeek,
+          $lt: endOfWeek,
+        },
+      },
+    },
+
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$dateOrder" } },
+        totalCustomer: { $sum: 1 },
+      },
+    },
+  ]).exec(function (err, doc) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
 module.exports = router;

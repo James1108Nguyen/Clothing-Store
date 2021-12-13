48420bf9305514deaa6b39f14112289358b035ce
HoQuangLinh <hoquanglinh2710@gmail.com>
2021-12-14 00:38:50 +0700

    up

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -11,8 +11,8 @@ router.get("/list", async (req, res) => {
   var orders = await Order.find()
     .populate({ path: "orderDetails" })
     .populate("customer", "name phone point")
-    .populate("user", "fullname")
-    .sort({ dateOrder: -1 });
+    .populate("user", "fullname");
+
   if (orders) {
     res.status(200).send(orders);
   } else {
